046be10f030ceed6f4e267f9b66be12758e55075
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-24 17:20:49 +0200

    Create NavLink.svelte

---
+++ src/lib/nav/NavLink.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	export let href: string;
+</script>
+
+<a
+	class="rounded-md hover:bg-fg-shade p-[0.6rem] transition relative group"
+	sveltekit:prefetch
+	{href}
+	class:bg-fg-shade={$page.url.pathname === href}
+>
+	<slot />
+</a>
