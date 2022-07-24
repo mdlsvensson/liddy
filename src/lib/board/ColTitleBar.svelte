f0d40917273a169ffe72cb68aa9eacd92d5f77e5
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-25 00:00:52 +0200

    Create ColTitleBar.svelte

---
+++ src/lib/board/ColTitleBar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Icon from '$lib/common/Icon.svelte';
+</script>
+
+<div class="flex items-center py-2 pl-4 pr-2 rounded-xl bg-fg place-content-between">
+	<h1><slot /></h1>
+	<button
+		class="w-6 fill-text-fg hover:bg-fg-shade rounded-md p-[2px] transition duration-100"
+		on:click
+	>
+		<Icon name="add" />
+	</button>
+</div>
