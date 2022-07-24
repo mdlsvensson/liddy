d6dab8e821c606460c66f0ac9f6034dea6af5680
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-24 17:20:43 +0200

    Create NavTooltip.svelte

---
+++ src/lib/nav/NavTooltip.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let bg: string;
+</script>
+
+<div
+	class="absolute invisible opacity-0 rounded bg-{bg} transition left-16 top-2 px-2 py-1 group-hover:opacity-100 group-hover:visible shadow"
+>
+	<slot />
+</div>
