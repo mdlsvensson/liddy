fc3b3967c36d0f21b43a9ef14b832f68ee70f205
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-25 00:01:37 +0200

    Added kanban columns

---
+++ src/routes/board.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Col from '$lib/board/Col.svelte';
+</script>
+
+<div class="h-full flex gap-6">
+	<Col title="Not Assigned" />
+	<Col title="Ready" />
+	<Col title="In Progress" />
+</div>
