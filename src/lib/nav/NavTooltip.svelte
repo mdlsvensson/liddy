69e37af8028cd6da7674571a645afd822bc6f8c3
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-24 21:06:02 +0200

    Fixed bg color bug

--- src/lib/nav/NavTooltip.svelte
+++ src/lib/nav/NavTooltip.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	export let bg: string;
+	export let color: string;
 </script>
 
 <div
-	class="absolute invisible opacity-0 rounded bg-{bg} transition left-16 top-2 px-2 py-1 group-hover:opacity-100 group-hover:visible shadow"
+	class="absolute invisible opacity-0 rounded {color} transition left-16 top-2 px-2 py-1 group-hover:opacity-100 group-hover:visible shadow"
 >
 	<slot />
 </div>
