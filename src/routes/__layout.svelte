cb1cf5bdbf26f19ac8aaaea9354928819ad3832e
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-25 00:01:11 +0200

    Added modal

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import '../app.css';
+
 	import Logo from '$lib/Logo.svelte';
 	import Nav from '$lib/nav/Nav.svelte';
+	import Modal from '$lib/common/Modal.svelte';
 </script>
 
 <svelte:head>
@@ -9,6 +11,8 @@
 </svelte:head>
 
 <div class="flex w-screen h-screen">
+	<Modal />
+
 	<header class="w-16 h-full bg-fg flex flex-col gap-6 items-center">
 		<Logo />
 		<Nav />
