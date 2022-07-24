8d97710c902c9abd6b8d4c67b18d0d4f8acdd01c
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-25 00:00:48 +0200

    Create Col.svelte

---
+++ src/lib/board/Col.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import ColTitleBar from './ColTitleBar.svelte';
+
+	export let title: string;
+
+	let inputToggled = false;
+
+	const handleClick = () => {
+		inputToggled = !inputToggled;
+	};
+</script>
+
+<div class="flex flex-col gap-3">
+	<ColTitleBar on:click={handleClick}>{title}</ColTitleBar>
+	<div class="min-w-[300px] max-w-[400px] h-full bg-fg rounded-xl flex flex-col" />
+</div>
