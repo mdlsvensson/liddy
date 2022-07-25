2b88dd55abac9af2e4693807ee78d64c83251c7e
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-25 20:31:34 +0200

    Create Modal.svelte

---
+++ src/lib/common/Modal.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { modal } from '$lib/store';
+	import { clickOutside } from '$lib/util/clickOutside';
+	import ModalContent from './ModalContent.svelte';
+
+	const handleClickOutside = () => {
+		$modal.isVisible = false;
+	};
+</script>
+
+<div
+	class="opacity-0 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-black p-6 dim"
+	class:opacity-100={$modal.isVisible}
+	use:clickOutside
+	on:click_outside={handleClickOutside}
+>
+	<ModalContent type={$modal.type} />
+</div>
