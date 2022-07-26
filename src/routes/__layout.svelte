132211d9f07767139c0fbfd171c3ed50d9775cde
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-26 16:13:53 +0200

    Moved the modal component out of the main wrapper

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,16 +3,16 @@
 
 	import Logo from '$lib/Logo.svelte';
 	import Nav from '$lib/nav/Nav.svelte';
-	import Modal from '$lib/common/Modal.svelte';
+	import Modal from '$lib/modal/Modal.svelte';
 </script>
 
 <svelte:head>
 	<title>Liddy</title>
 </svelte:head>
 
-<div class="flex w-screen h-screen">
-	<Modal />
+<Modal />
 
+<div class="flex w-screen h-screen">
 	<header class="w-16 h-full bg-fg flex flex-col gap-6 items-center">
 		<Logo />
 		<Nav />
