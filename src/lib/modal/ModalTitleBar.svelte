2c26ecaae8f89624a87f4dd1dfe83d24062b6601
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-26 16:13:31 +0200

    Create ModalTitleBar.svelte

---
+++ src/lib/modal/ModalTitleBar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { modal } from '$lib/store';
+	import Icon from '$lib/common/Icon.svelte';
+
+	export let title: string;
+
+	const handleClick = () => {
+		$modal.isVisible = false;
+	};
+</script>
+
+<div class="w-full flex place-content-between">
+	<h1 class="pl-2">{title}</h1>
+	<button
+		class="w-7 fill-text-fg hover:bg-fg-shade rounded-md p-[2px] transition duration-100"
+		on:click={handleClick}
+	>
+		<Icon name="close" />
+	</button>
+</div>
