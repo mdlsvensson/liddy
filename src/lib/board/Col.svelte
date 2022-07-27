d7c2627bfed321d05aa2a638c978707c1c676e3e
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-27 21:25:52 +0200

    New Card object, imported Card interface, typo

--- src/lib/board/Col.svelte
+++ src/lib/board/Col.svelte
@@ -1,16 +1,36 @@
 <script lang="ts">
 	import { modal } from '$lib/store';
+	import type { Card } from '$lib/interfaces';
 	import ColTitleBar from './ColTitleBar.svelte';
 
 	export let title: string;
 
-	const handleClick = () => {
+	const handleClickTitleBar = () => {
 		$modal.isVisible = true;
-		$modal.type = 'NewCard';
+		$modal.type = 'Card';
+
+		const newCard = <Card>{
+			id: '',
+			name: '',
+			description: '',
+			assignee: '',
+			createdAt: new Date().getTime().toString(),
+			updatedAt: new Date().getTime().toString(),
+			dueDate: '',
+			priority: 0,
+			weight: 1,
+			column: 'Not Assigned',
+			tags: [],
+			comments: [],
+			attachments: [],
+			projects: [],
+		};
+
+		$modal.data = newCard;
 	};
 </script>
 
 <div class="flex flex-col gap-3">
-	<ColTitleBar on:click={handleClick}>{title}</ColTitleBar>
+	<ColTitleBar on:click={handleClickTitleBar}>{title}</ColTitleBar>
 	<div class="min-w-[300px] max-w-[400px] h-full bg-fg rounded-xl flex flex-col" />
 </div>
