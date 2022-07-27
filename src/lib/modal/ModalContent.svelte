59bd8d7130bc24d3cd6d76d06e3dd1f5329168af
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-27 21:18:06 +0200

    KeyPress and submit func for enter key, default focus for modal open

--- src/lib/modal/ModalContent.svelte
+++ src/lib/modal/ModalContent.svelte
@@ -1,16 +1,31 @@
 <script lang="ts">
+	import { modal } from '$lib/store';
 	import ModalTitleBar from './ModalTitleBar.svelte';
+	import CardModal from './types/CardModal.svelte';
 
 	export let type: string;
+	export let data: any;
+
+	let popupFocusedElement: HTMLElement | undefined;
+
+	const onKeyPress = (event: KeyboardEvent) => {
+		if (event.key === 'Enter') fieldSubmit(document.activeElement);
+	};
+
+	const fieldSubmit = (element: Element | null) => {
+		if (element instanceof HTMLElement) element.blur();
+	};
+
+	$: if ($modal.isVisible && data.name === '') {
+		if (popupFocusedElement != undefined) {
+			popupFocusedElement.focus();
+		}
+	}
 </script>
 
-{#if type === 'NewCard'}
-	<div class="w-[60vw] h-[60vh] flex flex-col gap-4">
+{#if type === 'Card'}
+	<div class="w-[60vw] flex flex-col">
 		<ModalTitleBar title="New Card" />
-
-		<div class="w-full h-full pl-2">
-			<label class="" for="card-assign-input">Assign</label>
-			<input name="card-assign-input" id="card-assign-input" />
-		</div>
+		<CardModal bind:nameInput={popupFocusedElement} {data} on:keypress={onKeyPress} />
 	</div>
 {/if}
