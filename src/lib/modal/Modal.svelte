dfcc585e0d720fc09dff32bf59d20b8123c0bedf
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-27 21:19:43 +0200

    Passing modal data to modal content

--- src/lib/modal/Modal.svelte
+++ src/lib/modal/Modal.svelte
@@ -9,10 +9,10 @@
 </script>
 
 <div
-	class="opacity-0 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-fg p-4 dim rounded-lg"
+	class="opacity-0 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-fg dim rounded-lg"
 	class:opacity-100={$modal.isVisible}
 	use:clickOutside
 	on:click_outside={handleClickOutside}
 >
-	<ModalContent type={$modal.type} />
+	<ModalContent type={$modal.type} data={$modal.data} />
 </div>
