68cf43d7641240d23a596cb41b4a4d8e7f9efd01
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-28 19:45:58 +0200

    Fixed bug with close button hover

--- src/lib/modal/ModalTitleBar.svelte
+++ src/lib/modal/ModalTitleBar.svelte
@@ -12,7 +12,7 @@
 <div class="w-full flex place-content-between bg-fg-shade p-2">
 	<h1 class="pl-[0.4rem] pt-[0.2rem]">{title}</h1>
 	<button
-		class="w-7 fill-text-fg hover:bg-fg-shade rounded-md p-[2px] transition duration-100"
+		class="w-7 fill-text-fg hover:bg-fg-light rounded-md p-[2px] transition duration-100"
 		on:click={handleClick}
 	>
 		<Icon name="close" />
