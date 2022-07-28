6c171b5df2e0bf7ca42c24f02882e96b83d32da3
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-28 19:44:19 +0200

    Moved global keybinds to layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
 	import '../app.css';
+	import { modal } from '$lib/store';
+	import { page } from '$app/stores';
 
 	import Logo from '$lib/Logo.svelte';
 	import Nav from '$lib/nav/Nav.svelte';
 	import Modal from '$lib/modal/Modal.svelte';
+
+	// Global keybinds
+	if (typeof window !== 'undefined') {
+		document.onkeydown = (event: KeyboardEvent) => {
+			if (event.key === 'Escape') {
+				if ($modal.isVisible) {
+					$modal.isVisible = false;
+				}
+			}
+		};
+	}
 </script>
 
 <svelte:head>
