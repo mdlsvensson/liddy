67056489bd13953cd69feda4ec678604863c36c8
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-28 19:46:33 +0200

    Added dependency and status buttons

--- src/lib/modal/types/CardModal.svelte
+++ src/lib/modal/types/CardModal.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import CardWeight from '$lib/card/CardWeight.svelte';
+	import CardPriority from '$lib/card/CardPriority.svelte';
 
 	export let data: any;
 	export let nameInput: HTMLElement | undefined;
@@ -29,14 +29,6 @@
 	</div>
 
 	<div class="flex gap-2">
-		<label for="card-tags" class="sr-only">Add tags to this task</label>
-		<button
-			class="p-2 transition bg-fg-shade rounded-md w-full hover:bg-fg-light"
-			name="card-tags"
-			id="card-tags"
-			on:click|preventDefault>Tags</button
-		>
-
 		<label for="card-assign" class="sr-only">Assign someone to this task</label>
 		<button
 			class="p-2 transition bg-fg-shade rounded-md w-full hover:bg-fg-light"
@@ -45,6 +37,30 @@
 			on:click|preventDefault>Assign</button
 		>
 
-		<CardWeight weight={data.weight} />
+		<label for="card-tags" class="sr-only">Add tags to this task</label>
+		<button
+			class="p-2 transition bg-fg-shade rounded-md w-full hover:bg-fg-light"
+			name="card-tags"
+			id="card-tags"
+			on:click|preventDefault>Tags</button
+		>
+
+		<label for="card-dependencies" class="sr-only">Add dependencies to this task</label>
+		<button
+			class="p-2 transition bg-fg-shade rounded-md w-full hover:bg-fg-light"
+			name="card-dependencies"
+			id="card-dependencies"
+			on:click|preventDefault>Dependencies</button
+		>
+
+		<label for="card-status" class="sr-only">Set the status of this task</label>
+		<button
+			class="p-2 transition bg-fg-shade rounded-md w-full hover:bg-fg-light"
+			name="card-status"
+			id="card-status"
+			on:click|preventDefault>Status</button
+		>
+
+		<CardPriority priority={data.priority} />
 	</div>
 </form>
