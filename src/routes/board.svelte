2883a4c6e92d613ca7bd07f8f410c2b72c2dd4a5
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-30 13:56:24 +0200

    Made template columns dynamic

--- src/routes/board.svelte
+++ src/routes/board.svelte
@@ -1,11 +1,35 @@
 <script lang="ts">
+	import { state, columns } from '$lib/store';
+	import colorOptions from '$lib/util/colorOptions';
 	import Col from '$lib/board/Col.svelte';
+
+	const templateColumns = [
+		{
+			title: 'Backlog',
+			color: colorOptions.gray,
+		},
+		{
+			title: 'Unassigned',
+			color: colorOptions.red2,
+		},
+		{
+			title: 'In Progress',
+			color: colorOptions.yellow,
+		},
+		{
+			title: 'Done',
+			color: colorOptions.green2,
+		},
+	];
+
+	if ($state.isNewUser) {
+	}
 </script>
 
 <div class="h-full flex gap-6">
-	<Col title="Backlog" />
-	<Col title="Unassigned" />
-	<Col title="In Progress" />
-	<Col title="Testing" />
-	<Col title="Done" />
+	{#if $state.isNewUser}
+		{#each templateColumns as column}
+			<Col title={column.title} color={column.color} />
+		{/each}
+	{/if}
 </div>
