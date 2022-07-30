98f96c398bde150d70a182d7a6c82392f72cf422
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-30 18:50:16 +0200

    Changed order of interfaces

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -1,3 +1,46 @@
+export interface State {
+  isNewUser: boolean;
+  isLoading: boolean;
+  isError: boolean;
+  isLoggedIn: boolean;
+  isDatabaseConnected: boolean;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  isAdmin: boolean;
+  projects: Project[];
+  createdAt: number;
+  updatedAt: number;
+  lastLogin: number;
+  lastLoginIp: number;
+}
+
+export interface Project {
+  id: string;
+  name: string;
+  description: string;
+  members: User[];
+  columns: Column[];
+  isActive: boolean;
+  isArchived: boolean;
+  isDeleted: boolean;
+  isPublic: boolean;
+  isLocked: boolean;
+  createdAt: number;
+  updatedAt: number;
+  dueAt: number | null;
+}
+
+export interface Column {
+  id: string;
+  title: string;
+  color: string;
+  cards: Array<Card>;
+}
+
 export interface Card {
   id: string;
   name: string,
@@ -9,46 +52,13 @@ export interface Card {
   tags: Array<number>,
   comments: Array<string>,
   attachments: Array<string>,
-  projectId: string,
-  createdAt: string,
-  updatedAt: string,
-  dueAt: string,
-}
-
-export interface Column {
-  id: string;
-  title: string;
-  color: string;
-  cards: Array<Card>;
-}
-
-export interface Project {
-  id: string;
-  name: string;
-  description: string;
-  columns: Array<Column>;
-  isActive: boolean;
-  isArchived: boolean;
-  isDeleted: boolean;
-  isPublic: boolean;
-  isLocked: boolean;
-  createdAt: string;
-  updatedAt: string;
-  dueAt: string;
+  createdAt: number,
+  updatedAt: number,
+  dueAt: number | null,
 }
 
 export interface Modal {
   isVisible: boolean;
   type: string;
-  data: object;
+  data: Record<string, any>;
 }
-
-export interface State {
-  isNewUser: boolean;
-  isLoading: boolean;
-  isError: boolean;
-  isLoggedIn: boolean;
-  isDatabaseConnected: boolean;
-  project: object;
-}
-
