cd62698abe549d2bb0dc9fbf0b4aeffa72c557e4
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-30 13:51:27 +0200

    Setting the border color with inline style

--- src/lib/board/ColTitleBar.svelte
+++ src/lib/board/ColTitleBar.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
 	import Icon from '$lib/common/Icon.svelte';
+
+	export let color: string;
+
+	console.log(color);
 </script>
 
-<div class="flex items-center py-2 pl-4 pr-2 rounded-xl bg-fg place-content-between">
+<div
+	style="border-color: {color};"
+	class="flex items-center py-2 pl-4 pr-2 rounded-xl bg-fg place-content-between border-2"
+>
 	<h1><slot /></h1>
 	<button
 		class="w-6 fill-text-fg hover:bg-fg-shade rounded-md p-[2px] transition duration-100"
