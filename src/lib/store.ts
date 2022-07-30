d00024debc1f34ee25ba17b4baba775e9af67eff
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-30 13:55:54 +0200

    reorg stores, imported interfaces, added column

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,7 +1,8 @@
 import { writable, type Writable } from "svelte/store";
-import type { Modal, State } from "./interfaces";
+import type { Modal, State, Column, Project } from "./interfaces";
 
 export const state: Writable<State> = writable({
+  isNewUser: true,
   isLoading: false,
   isError: false,
   isLoggedIn: false,
@@ -21,20 +22,29 @@ export const user: Writable<Object> = writable({
   isAdmin: false,
 });
 
-export const projects: Writable<Array<Object>> = writable([
+export const project: Writable<Project> = writable(
   {
     id: "",
-    createdAt: "",
-    updatedAt: "",
     name: "",
     description: "",
+    columns: [],
     isActive: false,
     isArchived: false,
     isDeleted: false,
     isPublic: false,
     isLocked: false,
+    createdAt: "",
+    updatedAt: "",
+    dueAt: "",
   }
-]);
+);
+
+export const columns: Writable<Column> = writable({
+  id: "",
+  title: "",
+  color: "",
+  cards: [],
+});
 
 export const modal: Writable<Modal> = writable({
   isVisible: false,
