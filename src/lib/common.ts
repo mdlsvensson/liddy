7eb972441aa8595b538071e49e4857c901f2e1c5
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-31 19:47:15 +0200

    Added project and user factories

--- src/lib/common.ts
+++ src/lib/common.ts
@@ -1,17 +1,42 @@
-import type { Card } from "$lib/types";
+import type { Card, User, Project } from "$lib/types";
 import { id, descriptor, dated, dueDate, owner } from "$lib/factories";
 
-export const newCard = ({ ownerId }: { ownerId: string }): Card => {
-  return <Card>{
-    ...id({ id: "" }),
-    ...descriptor({ name: '', description: '' }),
-    ...dated({ createdAt: new Date().getTime(), updatedAt: new Date().getTime() }),
-    ...dueDate({ dueDate: null }),
-    ...owner({ ownerId }),
-    priority: 0,
-    weight: 0,
-    tags: [],
-    comments: [],
-    attachments: [],
-  }
-};
+export const newUser = (): User => ({
+  ...id({ id: "" }),
+  ...descriptor({ name: "", description: "" }),
+  ...dated({ createdAt: new Date().getTime(), updatedAt: new Date().getTime() }),
+  email: "",
+  isNewUser: true,
+  isAdmin: false,
+  projects: [],
+  lastLogin: new Date().getTime(),
+  lastLoginIp: null,
+});
+
+export const newProject = ({ ownerId }: { ownerId: string }): Project => ({
+  ...id({ id: "" }),
+  ...descriptor({ name: '', description: '' }),
+  ...dated({ createdAt: new Date().getTime(), updatedAt: new Date().getTime() }),
+  ...dueDate({ dueDate: null }),
+  ...owner({ ownerId }),
+  members: [],
+  columns: [],
+  isActive: false,
+  isArchived: false,
+  isDeleted: false,
+  isPublic: false,
+  isLocked: false,
+});
+
+export const newCard = ({ ownerId }: { ownerId: string }): Card => ({
+  ...id({ id: "" }),
+  ...descriptor({ name: '', description: '' }),
+  ...dated({ createdAt: new Date().getTime(), updatedAt: new Date().getTime() }),
+  ...dueDate({ dueDate: null }),
+  ...owner({ ownerId }),
+  priority: 0,
+  weight: 0,
+  tags: [],
+  comments: [],
+  attachments: [],
+});
