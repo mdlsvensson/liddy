0c48253f4295668b8422be3f62483b5c7660108f
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-01 16:06:04 +0200

    Added env

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,12 @@
-import postgres from 'postgres'
+import postgres from 'postgres';
+import 'dotenv/config';
 
 const sql = postgres('postgres://username:password@host:port/database', {
-  host                 : '',            // Postgres ip address[s] or domain name[s]
-  port                 : 5432,          // Postgres server port[s]
-  database             : '',            // Name of database to connect to
-  username             : '',            // Username of database user
-  password             : '',            // Password of database user
-})
+  host                 : process.env.PG_HOST,                               // Postgres ip address[s] or domain name[s]
+  port                 : parseInt(process.env.PG_PORT as string, 10),           // Postgres server port[s]
+  database             : process.env.PG_DATABASE,                           // Name of database to connect to
+  username             : process.env.PG_USERNAME,                           // Username of database user
+  password             : process.env.PG_PASSWORD,                           // Password of database user
+});
 
 export default sql
