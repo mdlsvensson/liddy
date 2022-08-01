64fad0d5da86f9b2560acf86162cd6f4834129e3
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-01 14:54:59 +0200

    Refactored and fixed reactivity bug

--- src/routes/board.svelte
+++ src/routes/board.svelte
@@ -1,35 +1,44 @@
 <script lang="ts">
-	import { state, column } from '$lib/store';
+	import { project } from '$lib/store';
+	import { newColumn } from '$lib/common';
 	import colorOptions from '$lib/util/colorOptions';
 	import Col from '$lib/board/Col.svelte';
 
-	const templateColumns = [
-		{
-			title: 'Backlog',
-			color: colorOptions.gray,
-		},
-		{
-			title: 'Unassigned',
-			color: colorOptions.red2,
-		},
-		{
-			title: 'In Progress',
-			color: colorOptions.yellow,
-		},
-		{
-			title: 'Done',
-			color: colorOptions.green2,
-		},
-	];
+	$: if ($project != undefined && $project.isNewProject) {
+		$project.columns = [
+			...$project.columns,
+			newColumn({
+				title: 'Backlog',
+				color: colorOptions.gray,
+			}),
+			newColumn({
+				title: 'Unassigned',
+				color: colorOptions.red2,
+			}),
+			newColumn({
+				title: 'In Progress',
+				color: colorOptions.yellow,
+			}),
+			newColumn({
+				title: 'Done',
+				color: colorOptions.green2,
+			}),
+		];
 
-	if ($state.isNewUser) {
+		$project.isNewProject = false;
 	}
 </script>
 
 <div class="h-full flex gap-6">
-	{#if $state.isNewUser}
-		{#each templateColumns as column}
-			<Col title={column.title} color={column.color} />
-		{/each}
+	{#if $project != undefined}
+		{#if $project.columns.length > 0}
+			{#each $project.columns as column}
+				<Col title={column.title} color={column.color} />
+			{/each}
+		{:else}
+			<button>Add Column</button>
+		{/if}
+	{:else}
+		<button>Create Project</button>
 	{/if}
 </div>
