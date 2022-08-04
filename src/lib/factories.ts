bb53a46bc67a8c11afa49bfce269315278f716f7
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-04 03:25:22 +0200

    Added, User, Project, Column, Card factories

--- src/lib/factories.ts
+++ src/lib/factories.ts
@@ -1,4 +1,4 @@
-import type { Id, Descriptor, Dated, DueDate, Owner, Title } from "$lib/types";
+import type { Id, Descriptor, Dated, DueDate, Owner, Title, User, Project, Column, Card } from "$lib/types";
 
 export const newId = ({ id }: { id: string }): Id => {
   return { id };
@@ -23,3 +23,54 @@ export const newDueDate = ({ dueDate }: { dueDate: number | null }): DueDate => 
 export const newOwner = ({ ownerId }: { ownerId: string }): Owner => {
   return { ownerId };
 };
+
+export const newUser = (): User => ({
+  ...newId({ id: "" }),
+  ...newDescriptor({ name: "", description: "" }),
+  ...newDated({ createdAt: new Date().getTime(), updatedAt: new Date().getTime() }),
+  email: "",
+  isNewUser: true,
+  isAdmin: false,
+  projects: [],
+  memberOf: [],
+  lastLogin: new Date().getTime(),
+  lastLoginIp: null,
+});
+
+export const newProject = ({ ownerId }: { ownerId: string }): Project => ({
+  ...newId({ id: "" }),
+  ...newDescriptor({ name: '', description: '' }),
+  ...newDated({ createdAt: new Date().getTime(), updatedAt: new Date().getTime() }),
+  ...newDueDate({ dueDate: null }),
+  ...newOwner({ ownerId }),
+  members: [],
+  columns: [],
+  isNewProject: true,
+  isActive: true,
+  isArchived: false,
+  isDeleted: false,
+  isPublic: false,
+  isLocked: false,
+});
+
+export const newColumn = ({ title, color, projectId }: { title: string, color: string, projectId: string }): Column => ({
+  ...newId({ id: "" }),
+  ...newTitle({ title }),
+  color,
+  cards: [],
+  projectId,
+});
+
+export const newCard = ({ ownerId, columnId }: { ownerId: string, columnId: string }): Card => ({
+  ...newId({ id: "" }),
+  ...newDescriptor({ name: '', description: '' }),
+  ...newDated({ createdAt: new Date().getTime(), updatedAt: new Date().getTime() }),
+  ...newDueDate({ dueDate: null }),
+  ...newOwner({ ownerId }),
+  priority: 0,
+  weight: 0,
+  tags: [],
+  comments: [],
+  attachments: [],
+  columnId,
+});
