dfaa8363af6da522244ab62aee4b627ff4b37d86
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-04 03:27:48 +0200

    Started rewrite of color scheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-const plugin = require('tailwindcss/plugin')
+const plugin = require('tailwindcss/plugin');
 
 const backfaceVisibility = plugin(function({addUtilities}) {
   addUtilities({
@@ -18,36 +18,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'bg': '#1F2430',
-        'fg': '#242936',
-        'fg-shade': '#2F3440',
-        'fg-light': '#393E4A',
-        'text-fg': '#E9EAEB',
-        'text-bg': '#E9E9EA',
-        'gray': '#CCCAC2',
-        'blue1': '#409FFF',
-        'blue2': '#73D0FF',
-        'blue3': '#5CCFE6',
-        'blue4': '#80BFFF',
-        'green1-dimmer': '#95B35A',
-        'green1-dim': '#B5D96D',
-        'green1': '#D5FF80',
-        'greed1-shade': '#DBFF93',
-        'green2': '#87D96C',
-        'green3': '#95E6CB',
-        'red1-dimmer': '#A9555C',
-        'red1-dim': '#CE676F',
-        'red1': '#F27983',
-        'red1-shade': '#F48D96',
-        'red2': '#F28779',
-        'red3': '#F29E74',
-        'yellow-dimmer': '#B39251',
-        'yellow-dim': '#D9B262',
-        'yellow': '#FFD173',
-        'yellow-shade': '#FFD888',
+        'bg1': '#1F2430',
+        'bg2': '#2A2F3A',
+        'bg3': '#353A45',
+        'bg4': '#41454F',
+        'bg5': '#4C5059',
+        'text': '#EAE8FF',
+        'red': '#B10F2E',
+        'green': '#26c485',
+        'blue': '#0094C6',
         'orange': '#FFAD66',
-        'beige': '#FFDFB3',
-        'purple': '#DFBFFF',
+        'orange-dim': '#B37947',
       },
     },
   },
