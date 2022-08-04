fe39039586d2efa4cda45bdd3faecd43a9ea5f40
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-04 03:23:46 +0200

    Create stores.ts

---
+++ src/lib/stores.ts
@@ -0,0 +1,28 @@
+import { writable, type Writable } from "svelte/store";
+import type { Modal, State, Column, Project, User } from "./types";
+
+export const state: Writable<State> = writable({
+  isNewUser: true,
+  isLoading: false,
+  isError: false,
+  isLoggedIn: false,
+  isDatabaseConnected: false,
+});
+
+export const user: Writable<User> = writable();
+
+export const project: Writable<Project> = writable();
+
+export const column: Writable<Column> = writable({
+  id: "",
+  title: "",
+  color: "",
+  cards: [],
+});
+
+export const modal: Writable<Modal> = writable({
+  isVisible: false,
+  type: null,
+  mode: null,
+  data: {},
+});
