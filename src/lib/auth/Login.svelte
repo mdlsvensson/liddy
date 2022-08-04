a9ead261c7b506786859a977dd7b93b4c895513f
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-04 11:06:08 +0200

    removed comment

--- src/lib/auth/Login.svelte
+++ src/lib/auth/Login.svelte
@@ -20,15 +20,14 @@
 </script>
 
 <div class="w-full h-full flex justify-center items-center">
-	<footer class="absolute bottom-4 left-1/2 -translate-x-1/2">
-		<a href="https://github.com/mdlsvensson/liddy" class="fill-text"><Icon name="github" /></a>
-	</footer>
+	<Icon name="discord" />
+	<Icon name="github" />
+
 	<form class="w-[350px] flex flex-col gap-4 on:submit|preventDefault={handleLogin}">
 		<input
 			class="pb-4 px-6 pt-[17px] w-full bg-bg2 rounded-md hover:ring-2 ring-bg3"
 			type="email"
 			placeholder="Your email"
-			bind:value={email}
 		/>
 
 		<div class="w-full h-14">
@@ -41,88 +40,7 @@
 		</div>
 	</form>
 </div>
-<!-- 
-	$bg: #fff;
-	$text: #382b22;
-	$light-pink: #fff0f0;
-	$pink: #ffe9e9;
-	$dark-pink: #f9c4d2;
-	$pink-border: #b18597;
-	$pink-shadow: #ffe3e2;
 
-	* {
-		box-sizing: border-box;
-		&::before,
-		&::after {
-			box-sizing: border-box;
-		}
-	}
-
-	body {
-		font-family: 'Rubik', sans-serif;
-		font-size: 1rem;
-		line-height: 1.5;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		margin: 0;
-		min-height: 100vh;
-		background: $bg;
-	}
-
-	button {
-		position: relative;
-		display: inline-block;
-		cursor: pointer;
-		outline: none;
-		border: 0;
-		vertical-align: middle;
-		text-decoration: none;
-		font-size: inherit;
-		font-family: inherit;
-		&.learn-more {
-			font-weight: 600;
-			color: $text;
-			text-transform: uppercase;
-			padding: 1.25em 2em;
-			background: $light-pink;
-			border: 2px solid $pink-border;
-			border-radius: 0.75em;
-			transform-style: preserve-3d;
-			transition: transform 150ms cubic-bezier(0, 0, 0.58, 1),
-				background 150ms cubic-bezier(0, 0, 0.58, 1);
-			&::before {
-				position: absolute;
-				content: '';
-				width: 100%;
-				height: 100%;
-				top: 0;
-				left: 0;
-				right: 0;
-				bottom: 0;
-				background: $dark-pink;
-				border-radius: inherit;
-				box-shadow: 0 0 0 2px $pink-border, 0 0.625em 0 0 $pink-shadow;
-				transform: translate3d(0, 0.75em, -1em);
-				transition: transform 150ms cubic-bezier(0, 0, 0.58, 1),
-					box-shadow 150ms cubic-bezier(0, 0, 0.58, 1);
-			}
-			&:hover {
-				background: $pink;
-				transform: translate(0, 0.25em);
-				&::before {
-					box-shadow: 0 0 0 2px $pink-border, 0 0.5em 0 0 $pink-shadow;
-					transform: translate3d(0, 0.5em, -1em);
-				}
-			}
-			&:active {
-				background: $pink;
-				transform: translate(0em, 0.75em);
-				&::before {
-					box-shadow: 0 0 0 2px $pink-border, 0 0 $pink-shadow;
-					transform: translate3d(0, 0, -1em);
-				}
-			}
-		}
-	}
- -->
+<footer class="absolute bottom-0 left-1/2 -translate-x-1/2 w-12">
+	<a href="https://github.com/mdlsvensson/liddy" class="fill-text"><Icon name="github2" /></a>
+</footer>
