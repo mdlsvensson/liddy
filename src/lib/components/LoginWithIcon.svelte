9b4cd4c73eba789314f07bfb5c06fbf3514f34ef
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-06 00:17:17 +0200

    Tooltip and capitalize

--- src/lib/components/LoginWithIcon.svelte
+++ src/lib/components/LoginWithIcon.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import Icon from './Icon.svelte';
+	import { tooltipToggle } from '$lib/tooltip';
+	import { capitalize } from '$lib/common';
 
 	export let name: string;
 </script>
 
-<button class="fill-text1 hover:fill-text2 transition">
+<button
+	class="fill-text1 hover:fill-text2 transition"
+	use:tooltipToggle
+	data-tooltip={capitalize(name)}
+>
 	<Icon {name} />
 </button>
