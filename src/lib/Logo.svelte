8bd2c2db97e7908bfde4f1d6974e5247924723c9
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-05 13:04:57 +0200

    restructuring

--- src/lib/Logo.svelte
+++
@@ -1,3 +0,0 @@
-<div class="w-full h-16 flex justify-center items-center mt-2">
-	<div class="bg-white w-10 h-10 rounded-3xl" />
-</div>
--- src/lib/nav/NavTooltip.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let color: string;
-</script>
-
-<div
-	class="absolute invisible opacity-0 rounded {color} transition left-16 top-2 px-2 py-1 group-hover:opacity-100 group-hover:visible shadow"
->
-	<slot />
-</div>
