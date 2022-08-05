007eb0790271e7da6ee286caaba5337bbe977440
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-06 00:16:01 +0200

    Create LoginLinkButton.svelte

---
+++ src/lib/components/LoginLinkButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { tooltipToggle } from '$lib/tooltip';
+
+	export let loading: boolean;
+	export let buttonText: string;
+</script>
+
+<div class="w-full h-14">
+	<input
+		type="submit"
+		value={loading ? 'Loading' : buttonText}
+		disabled={loading}
+		class="h-full bg-orange rounded-md w-full text-bg1 font-bold text-bg border-orange-dim border-b-4 hover:border-b-0 hover:translate-y-[4px] hover:h-[52px] transition-all cursor-pointer duration-100"
+		use:tooltipToggle
+		data-tooltip="Send login link to your email, no password required."
+	/>
+</div>
