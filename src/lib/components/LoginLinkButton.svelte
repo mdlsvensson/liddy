2d140d5774fe702a2789e1c52a138771bccc60d0
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-08-06 18:58:38 +0200

    Changed tooltip text

--- src/lib/components/LoginLinkButton.svelte
+++ src/lib/components/LoginLinkButton.svelte
@@ -12,6 +12,6 @@
 		disabled={loading}
 		class="h-full bg-orange rounded-md w-full text-bg1 font-bold text-bg border-orange-dim border-b-4 hover:border-b-0 hover:translate-y-[4px] hover:h-[52px] transition-all cursor-pointer duration-100"
 		use:tooltipToggle
-		data-tooltip="Send login link to your email, no password required."
+		data-tooltip="Login with magic link"
 	/>
 </div>
